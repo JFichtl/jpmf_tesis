a7c14d5366f5bea1a052b057d6e024dd73773e77
JFichtl <jpmfichtl@gmail.com>
2023-03-11 15:24:18 -0600

    Incorporación de métodos a la clase de núcleos.

--- nucleos.py
+++ nucleos.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.ndimage import convolve
 from matplotlib import pyplot as plt
 
-trad = {'dirichlet':'Dirichlet', 'fejer':'Fejér', 'poisson':'Poisson', 'heat':'Calor'}
+trad = {'dirichlet':'Dirichlet', 'fejer':'Fejér', 'poisson':'Poisson', 'heat':'calor'}
 
 def hamming_dist(w1, w2):
     A = set(w1)
@@ -11,25 +11,6 @@ def hamming_dist(w1, w2):
     m = len(A.symmetric_difference(A))
     k = len(A.union(B))
     return n/k
-
-def ker_generator(name, a, b, n, vals):
-    A = b+a
-    B = b-a
-    f   = 2*np.pi/B
-    phi = np.pi*A/B
-    x = f * vals - phi
-    if name == 'fejer':
-        return (np.sin((n+1)* x *0.5) / np.sin(0.5* x ) )**2 / (B*(1+n))
-    elif name == 'dirichlet':
-        return  (1/B) * np.sin((n+0.5)* x)/np.sin(0.5* x )
-    elif name == 'poisson':
-        # add a regularization constant to avoid division by zero,
-        # define a sequence n that goes to 1 as n grows
-        r = 1 - (1/(n+1)) + 1e-6
-        return  (1/B) * (1-pow(r,2))/(1-2*r*np.cos(x) + pow(r,2))
-    elif name == 'heat':
-        t = 1/(n+1)
-        return (1/pow(4*np.pi*t, 0.5)) * np.exp(-pow(x/pow(4*t, 0.5) , 2))
 
 class kernel: 
     def __init__(self, name, a, b, n, points=1000): 
@@ -48,14 +29,33 @@ class kernel:
         self.delta = self.t[1] - self.t[0]
     
         if self.name in trad.keys():
-           self.values = ker_generator(self.name, self.a, self.b, self.n, self.t) 
+           self.values = self.ker_generator(self.n) 
         else:
             comparison = [hamming_dist(self.name, w2) for w2 in trad.keys() ]
             m = max(comparison)
             likely = list(trad.keys())[comparison.index(m)]
             print(f"I'm guessing you meant {likely}.")
             self.name = likely
-            self.values = ker_generator(self.name, self.a, self.b, self.n, self.t)
+            self.values = self.ker_generator(self.n)
+            
+    def ker_generator(self, n):
+        A = self.b + self.a
+        B = self.b - self.a
+        f   = 2*np.pi/B
+        phi = np.pi*A/B
+        x = f * self.t - phi
+        if self.name == 'fejer':
+          return (np.sin((n + 1)* x *0.5) / np.sin(0.5* x ) )**2 / (B*(1+n))
+        elif self.name == 'dirichlet':
+           return  (1/B) * np.sin((n+0.5)* x)/np.sin(0.5* x )
+        elif self.name == 'poisson':
+            # add a regularization constant to avoid division by zero,
+            # define a sequence n that goes to 1 as n grows
+            r = 1 - (1/(n+1)) + 1e-6
+            return  (1/B) * (1-pow(r,2))/(1-2*r*np.cos(x) + pow(r,2))
+        elif self.name == 'heat':
+            t = 1/(n+1)
+            return (1/pow(4*np.pi*t, 0.5)) * np.exp(-pow(x/pow(4*t, 0.5) , 2))
 
 
     def convolve(self, array, show = False, null=False, mode = 'circular'):
@@ -67,31 +67,32 @@ class kernel:
             t_conv  = np.linspace(2*self.a, 2*self.b, 2*len(array) -1)
             convolution = self.delta * np.convolve(self.values, array)
         if show:
-            plt.plot( self.t, array, label = 'Initial f.')
+            plt.plot( self.t, array, label = 'Función inicial f.')
             if not null:
-                plt.plot( t_conv,  convolution, label = f"{self.n}th order approx.")
+                plt.plot( t_conv,  convolution, label = f"Aproximación de orden {self.n}.")
                 plt.legend()
+                plt.title(f"Convolución del núcleo de {trad[self.name]} con f.")
                 plt.show()
         else:
             return t_conv, convolution
+            
+    def approximate(self, array, start=1, end=10, skip=1):
+        mode = 'circular'
+        if self.name == 'heat':
+            mode = 'linear'
+        
+        self.convolve(array, show=True, null = True, mode=mode)
+        for i in range(start, end+skip, skip):
+            self.values = self.ker_generator(i)
+            x, y = self.convolve(array, mode=mode)
+            plt.plot(x, y, label = f"k = {i}", alpha = 1 - 0.5*(i/end) )
+        self.values = self.ker_generator(self.n)
+        plt.legend()
+        plt.show()
+        
     def show(self):
         plt.plot(self.t, self.values)
         plt.show()
-
-def approximate_by_kernel(array, a, b, start=1, end=10, skip=1, ker_name='dirichlet'):
-    mode = 'circular'
-    if ker_name in ['dirichlet', 'fejer', 'poisson', 'heat']:
-        if ker_name == 'heat':
-            mode = 'linear'
-        kernel(ker_name, a, b, 0).convolve(array, show=True, null = True, mode=mode)
-        for i in range(start, end+skip, skip):
-           Ker = kernel(ker_name, a, b, i)
-           x, y = Ker.convolve(array, mode=mode)
-           plt.plot(x, y, label = f"k = {i}", alpha = 1 - 0.5*(i/end) )
-        plt.legend()
-        plt.show()
-    else:
-        raise("Invalid kernel")
 
 def showcase_kernel(a=-np.pi, b=np.pi, start=1, end=10, skip=1, ker_name = 'dirichlet'):
     if ker_name in ['dirichlet', 'fejer', 'poisson', 'heat']:
